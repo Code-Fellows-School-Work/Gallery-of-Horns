a01c20b2b63ee86828190ffdc4b5416479304fed
Errol Vidad <errolvidad@gmail.com>
2023-11-28 03:13:36 +0000

    mcorrected file location and added gallery, header and footer components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <footer className={styles.foot}>
+      &copy; 2023 Errol Vidad
+    </footer>
+  )
+}
+
+export default Footer;
