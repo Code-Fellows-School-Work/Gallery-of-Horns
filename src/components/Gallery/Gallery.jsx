503f538ca5c3c790899f98e75a2e359c7009cc0d
Errol Vidad <errolvidad@gmail.com>
2023-11-30 00:04:11 +0000

    display cimages in 3 columns with dynamic size

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -3,23 +3,30 @@ import React from 'react';
 
 import HornedBeast from '../HornedBeast/HornedBeast.jsx';
 import images from '../../assets/data.json';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 // used ChatGPT to write this function
 function Gallery() {
+  
   return (
-    <div>
-    {images.map((image) => (
-      <HornedBeast
-        key={image._id}
-        title={image.title}
-        image_url={image.image_url}
-        description={image.description}
-        keyword={image.keyword}
-        horns={image.horns}
-      />
-    ))}
-  </div>
-  )
+    <Container fluid>
+      <Row xs={1} sm={2} md={3} lg={3} xl={3}>
+        {images.map((image) => (
+          <Col key={image._id} className="mb-4">
+            <HornedBeast
+              title={image.title}
+              image_url={image.image_url}
+              description={image.description}
+              keyword={image.keyword}
+              horns={image.horns}
+            />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 }
 
 export default Gallery;
--- src/components/HornedBeast/HornedBeast.jsx
+++ src/components/HornedBeast/HornedBeast.jsx
@@ -5,28 +5,24 @@ import { FaHeart } from 'react-icons/fa';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // used ChatGPT to determine how to combine h2, img and p tag in one div
 // used ChatGPT to write the props.title, img src, and description
 // used ChatGPT to write container fluid and to style images to 10% width
+// used ChatGPT to troubleshoot how to display images in 3 columns and dynamically render based on screen size
 function HornedBeast(props) {
-  // Initialize the state variable for favorites
   const [favorites, setFavorites] = useState(0);
-
-  // Function to handle the favorite button click
   const handleFavoriteClick = () => {
-    // Update the favorites count when the button is clicked
     setFavorites(prevFavorites => prevFavorites + 1);
   };
 
   return (
-    <Container fluid>
+    <Container>
       <Row>
-        <Col>
-          <div>
-            <h2>{props.title}</h2>
-            <div onClick={handleFavoriteClick} style={{ cursor: 'pointer' }}>
-              <img
+        <Col sm={4}><h2>{props.title}</h2>
+             <div onClick={handleFavoriteClick} style={{ cursor: 'pointer' }}>
+               <img
                 src={props.image_url}
                 alt={props.title}
                 className="img-fluid"
@@ -36,13 +32,34 @@ function HornedBeast(props) {
             <p>{props.description}</p>
             <p>
               <FaHeart /> {favorites} Favorites
-            </p>
-          </div>
-        </Col>
-        
+            </p></Col>
       </Row>
     </Container>
   );
 }
+//   return (
+//     <Container fluid>
+//       <Row>
+//         <Col xs={12} sm={6} md={4}>
+//             <h2>{props.title}</h2>
+//             <div onClick={handleFavoriteClick} style={{ cursor: 'pointer' }}>
+//               <img
+//                 src={props.image_url}
+//                 alt={props.title}
+//                 className="img-fluid"
+//                 style={{ width: '10%', height: 'auto' }}
+//               />
+//             </div>
+//             <p>{props.description}</p>
+//             <p>
+//               <FaHeart /> {favorites} Favorites
+//             </p>
+//         </Col>
+//       </Row>
+//     </Container>
+
+    
+//   );
+// }
 
 export default HornedBeast;
